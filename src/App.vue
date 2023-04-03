a47f02652aae08443531674b65c1d4a15e06451e
Afnan Shaikh <48408572+IamAfnanSk@users.noreply.github.com>
2023-04-03 16:17:35 +0530

    style fix

--- src/App.vue
+++ src/App.vue
@@ -51,9 +51,27 @@ function deleteLocation(idx) {
 <style scoped>
 .weather-card-container {
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-columns: 1fr;
   gap: 20px;
   padding: 30px;
+}
+
+@media only screen and (min-width: 700px) {
+  .weather-card-container {
+    grid-template-columns: 1fr 1fr;
+  }
+}
+
+@media only screen and (min-width: 1200px) {
+  .weather-card-container {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+}
+
+@media only screen and (min-width: 1600px) {
+  .weather-card-container {
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+  }
 }
 
 .modal-wrapper {
